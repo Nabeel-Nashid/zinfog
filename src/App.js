5cd66d8e2404d14e27753e0d50bf3f4984b8cb99
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-06-16 02:45:57 +0530

    footer complete 16/06

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './components/Login';
+import LoginError from './components/LoginError';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/loginerror" element={<LoginError />} />
+          <Route path="/footer" element={<Footer />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="">
+      <div>Copyright © 2023 Access Home Lab Solutions </div>
+      <div>
+            All Rights Reserved |
+        <a href="">
+          <span className="text-[#2a2a9e]">Terms of service</span>
+        </a>{" "}
+        |{" "}
+        <a href="">
+          |<span className="text-[#2a2a9e]">Privacy policy</span>
+        </a>
+        |
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
