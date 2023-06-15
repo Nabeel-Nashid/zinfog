9fa4e0021ebfecffcd11f6f523f58aa6cf4b1bcb
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-06-15 23:29:19 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import bg from "../asset/bgImage.jpg";
+
+function Login() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-blue-200 to-cyan-200">
+      <div className="absolute top-[91px] left-[30%] w-[50%] h-[70%] rounded-[45px] bg-white ">
+            <h1>Zinfog</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
